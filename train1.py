507ee1acccdaff753518e021a699bd277a57bb26
Nay776 <195955212@qq.com>
2025-10-28 16:07:42 +0800

    Update train1.py

--- train1.py
+++ train1.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
 
     model.load('yolov8x.pt')
     train_results = model.train(
-        data="C:/Users/19595/Desktop/Analogical-Reasoning-main/VisDrone_Dataset_COCO_Format/VisDrone.yaml",
-        epochs=100,        # 修改：从80改为100
-        imgsz=1280,        # 修改：从1536改为1280
-        resume=True,
+        data="VisDrone_Dataset_COCO_Format/VisDrone.yaml",
+        epochs=10,        # 快速测试
+        imgsz=384,        # 小尺寸
+        fraction=0.05,    # 5%数据
         device=[0],
-        batch=1,           
+        batch=1,         
         lr0=0.01,          # 新增：初始学习率
         weight_decay=0.0005,  # 新增：权重衰减
         warmup_epochs=3    # 新增：预热轮数
