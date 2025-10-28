1abe6a979519a14c2379cbf70f5f756061ef7015
Nay776 <195955212@qq.com>
2025-10-28 13:37:58 +0800

    Update predict.py

--- Analogical/predict.py
+++ Analogical/predict.py
@@ -3,10 +3,10 @@ from ultralytics import SPARYOLO
 import cv2
 
 # 加载模型
-model = SPARYOLO("/home/lihua_zhou/nianxin/aaai/runs/detect/train12/weights/best.pt")  # 可换成 yolov8s.pt, yolov8m.pt, yolov8l.pt, yolov8x.pt
+model = SPARYOLO("C:/Users/19595/Desktop/Analogical-Reasoning-main/runs/detect/train12/weights/best.pt")  # 可换成 yolov8s.pt, yolov8m.pt, yolov8l.pt, yolov8x.pt
 
 # 读取图像
-image_path = "/home/lihua_zhou/nianxin/dataset/visdrone/VisDrone2019-DET-val/images/0000333_02941_d_0000016.jpg"
+image_path = "C:/Users/19595/Desktop/Analogical-Reasoning-main/VisDrone_Dataset_COCO_Format/images/0000333_02941_d_0000016.jpg"
 img = cv2.imread(image_path)
 
 # 检测图像
